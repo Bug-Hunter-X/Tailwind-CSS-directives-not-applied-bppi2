44494e10d0ef23bd1e9e1636d8590e03825eb9be
BugHunter-X <askzenai@gmail.com>
2025-02-08 16:06:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,10 @@
+```javascript
+// some javascript code that use tailwindcss classes
+// import the tailwindcss configuration
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+div {
+  text-center;
+}
+```
